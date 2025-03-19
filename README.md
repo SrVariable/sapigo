d03b940b6d7068c9cdcca87d0f6b9805e28683c0
SrVariable <rojohnibana@gmail.com>
2025-03-19 15:02:41 +0100

    Implement simple student API

---
+++ README.md
@@ -0,0 +1,9 @@
+# sapigo
+
+Simple API made in Go. Toy project to test how [gin](https://gin-gonic.com/) works.
+
+# References
+
+- https://go.dev/doc/tutorial/web-service-gin
+- https://youtu.be/67yGbvyM1is
+- https://gin-gonic.com/docs
---
+++ cmd/sapigo/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	defaultHost = "localhost"
+	defaultPort = "8080"
+	defaultUri  = defaultHost + ":" + defaultPort
+)
+
+type student struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var students = []student{
+	{ID: "1", Name: "Ana", Age: 18},
+	{ID: "2", Name: "Ben", Age: 20},
+	{ID: "3", Name: "Casey", Age: 22},
+	{ID: "4", Name: "Denise", Age: 24},
+	{ID: "5", Name: "Elmo", Age: 23},
+}
+
+func getStudent(c *gin.Context) {
+	c.JSON(http.StatusOK, students)
+}
+
+func getStudentByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, student := range students {
+		if student.ID == id {
+			c.JSON(http.StatusOK, student)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "student not found"})
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/students", getStudent)
+	router.GET("/students/:id", getStudentByID)
+
+	router.Run(defaultUri)
+}
